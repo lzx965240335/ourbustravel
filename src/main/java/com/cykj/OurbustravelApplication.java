76833b1f2a3e60bf3144aa7603437350235ef883
lzx965240335 <1666291251@qq.com>
2020-12-29 09:35:17 +0800

    框架

---
+++ src/main/java/com/cykj/OurbustravelApplication.java
@@ -0,0 +1,13 @@
+package com.cykj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurbustravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OurbustravelApplication.class, args);
+    }
+
+}
