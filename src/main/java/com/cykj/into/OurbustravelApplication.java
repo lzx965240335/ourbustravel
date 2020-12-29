a3c93f9c21c7d833b3bae2f07f8620dc6ec7a8bc
lzx965240335 <1666291251@qq.com>
2020-12-29 16:46:38 +0800

    原始

--- src/main/java/com/cykj/into/OurbustravelApplication.java
+++ src/main/java/com/cykj/into/OurbustravelApplication.java
@@ -1,4 +1,4 @@
-package com.cykj;
+package com.cykj.into;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @MapperScan("com.cykj.mapper")
 @ComponentScan({"com.cykj"})
+
 public class OurbustravelApplication {
 
     public static void main(String[] args) {
